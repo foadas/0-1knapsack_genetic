5218ec7e7b436eda54366447a1f2e2f1051a4a32
AminCht <amincht81@gmail.com>
2023-12-25 16:15:02 +0330

    add population function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 def read_csv():
     file_path = 'backpack_objects.csv'
@@ -10,7 +11,22 @@ def read_csv():
             objects.append(row)
     return objects
 
+def population(objects):
+    i = 0
+    population = []
+    while(i<200):
+        j = 0
+        selections = []
+        while(j<len(objects)):
+            selections.append(random.randrange(2))
+            j+=1
+        population.append(selections)
+        i+=1
+
+    print(population)
 
 if __name__ == '__main__':
     objects = read_csv()
+    population = population(objects)
+
     
